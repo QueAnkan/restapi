72a9de5b9933477c29fe8db94f0fea7614773bda
AnsoMalm <annsophie.malmqvist85@hotmail.com>
2023-05-25 12:18:20 +0200

    added router/get to users

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,41 @@ import express from 'express'
 import { getDb } from '../data/database.js'
 
 const router = express.Router()
-//const db = getDb()
+const db = getDb()
 
+router.get('/', async (req, res) => {
+	console.log('GET /users: ')
+	await db.read()
+	res.send(db.data.users)
+})
 
+router.get('/:id', async (req, res) => {
+	console.log('GET /users/:id')
+	if(!isValidId(req.params.id) ) {
+		res.sendStatus(400) //Bad request
+		return
+	} 
+	let usersId = Number(req.params.id)
 
+	await db.read()
+	let mayBeUsers = db.data.users.find(user => user.id === usersId)
+	if(!mayBeUsers) {
+		res.sendStatus(404) //not found
+		return
+	}
+	res.send(mayBeUsers)
 
+	
+
+})
+
+function isValidId(user) {
+	let maybeId = Number(user) 
+	if(isNaN(maybeId) ) {
+		return false 
+	}
+	return maybeId >= 0  
+} 
 
 
 export default router
