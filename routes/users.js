0b93b11642ec9da930fea0edcbc2796e18d64897
AnsoMalm <annsophie.malmqvist85@hotmail.com>
2023-05-25 15:42:09 +0200

    added post for users

--- routes/users.js
+++ routes/users.js
@@ -16,19 +16,62 @@ router.get('/:id', async (req, res) => {
 		res.sendStatus(400) //Bad request
 		return
 	} 
-	let usersId = Number(req.params.id)
+	let id = Number(req.params.id)
 
 	await db.read()
-	let mayBeUsers = db.data.users.find(user => user.id === usersId)
+	let mayBeUsers = db.data.users.find(user => user.id === id)
 	if(!mayBeUsers) {
 		res.sendStatus(404) //not found
 		return
 	}
 	res.send(mayBeUsers)
 
-	
+})
+
+router.post('/', async (req, res) => {
+	let mayBeUsers = req.body
+	console.log('Incoming user: ' , mayBeUsers)
+
+	if (isValidUser(mayBeUsers) ) {
+		await db.read()	
+		mayBeUsers.id = generateUserId()
+		console.log('Genereated ID: ', mayBeUsers.id )
+		db.data.users.push(mayBeUsers) 
+		await db.write()
+		res.status(200).json({ id: mayBeUsers.id})
+
+	} else {
+		res.sendStatus(400) //Bad request
+	}
 
 })
+
+function generateUserId() {
+	const highestId = db.data.users.reduce((maxId, currentUser) => {
+		return Math.max(maxId, currentUser.id) 
+	}, 0)
+	
+	return highestId + 1 
+	
+}
+
+function isValidUser(u) {
+	if ((typeof u) !== 'object') {
+		return false
+	} else if ((u === null) ) {
+		return false
+	}
+	let nameIsValid = (typeof u.name) === 'string'
+	nameIsValid = nameIsValid && u.name !== ''
+
+	let passwordIsValid = (typeof u.password) === 'string'
+	passwordIsValid = passwordIsValid && u.password !== ''
+
+	if(!nameIsValid || !passwordIsValid ) {
+		return false
+	}
+	return true
+}
 
 function isValidId(user) {
 	let maybeId = Number(user) 
