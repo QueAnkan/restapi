5a135e4e041ce524154417aec96d5394690b1ce7
QueAnka <annika.nylin@hotmail.com>
2023-05-30 10:56:15 +0200

    search variable changed

--- routes/search .js
+++ routes/search .js
@@ -4,26 +4,25 @@ import { getDb } from '../data/database.js'
 
 const router = express.Router()
 const db = getDb()
-console.log();db.data
+console.log(db.data);
 //hämta produkter utifrån tags eller name
 
 
 //TODO: *ändra params till query
 // 		*kunna söka på name också?
 router.get('/:tags' , async (req, res) => {
-	let possibleTag = req.params.tags
-console.log('this is apossible tag:', possibleTag);
+	let possibleSearch = req.params.tags
+console.log('this is apossible tag:', possibleSearch);
 	await db.read()
 
 
-	let possibleHats = db.data.products.filter(products => products.tags.some( tag => tag === possibleTag ))
-
+	let possibleHats = db.data.products.filter(products => products.tags.some( tag => tag === possibleSearch)) 
 	if(!possibleHats){
 		sendStatus(404)
 	}
 console.log('possible Hats ',possibleHats);
 	res.send(possibleHats)
-
+	
 })
 
 export default router
